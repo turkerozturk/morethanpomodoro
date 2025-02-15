bdf1108e49e25e4b6179b253bfe3d7f557f9a18d
turkerozturk <turker_ozt@yahoo.com>
2025-02-15 15:24:25 +0300

    gui: update about panel, add logo.png.

--- src/main/java/org/example/jpanels/about/AboutPanel.java
+++ src/main/java/org/example/jpanels/about/AboutPanel.java
@@ -4,6 +4,7 @@ import org.example.initial.ConfigManager;
 import org.example.initial.LanguageManager;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.net.URI;
 import java.util.ResourceBundle;
@@ -11,19 +12,43 @@ import java.util.ResourceBundle;
 public class AboutPanel extends JPanel {
     LanguageManager bundle = LanguageManager.getInstance();
     ConfigManager props = ConfigManager.getInstance();
+
+
     public AboutPanel() {
+        //this.logoIcon = new ImageIcon(getClass().getClassLoader().getResource("logo.png")); // Logonun yolu
 
-        setLayout(new GridLayout(10, 1, 5, 5));
+        /*
+        ImageIcon originalIcon = new ImageIcon(getClass().getClassLoader().getResource("logo.png"));
+        Image scaledImage = originalIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        ImageIcon logoIcon = new ImageIcon(scaledImage);
+        */
+        ImageIcon logoIcon = new ImageIcon(getClass().getClassLoader().getResource("logo.png"));
 
-        add(new JLabel(bundle.getString("app.name")));
-        add(new JLabel(bundle.getString("app.version")));
-        add(new JLabel(bundle.getString("app.date")));
-        add(new JLabel(bundle.getString("app.description")));
-        add(new JLabel(bundle.getString("contact.name")));
-       // add(createLinkLabel(bundle.getString("contact.email"), "mailto:" + bundle.getString("contact.email")));
-        add(createLinkLabel(bundle.getString("contact.github"), bundle.getString("contact.github")));
-       // add(createLinkLabel(bundle.getString("contact.help"), bundle.getString("contact.help")));
-       // add(createLinkLabel(bundle.getString("usermanual"), "file:./usermanual.pdf"));
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(10, 1, 5, 5));
+        // Ust tarafta 20 piksel bsşluk birak
+        panel.setBorder(new EmptyBorder(40, 0, 0, 0));
+        panel.add(new JLabel(bundle.getString("app.name")));
+        panel.add(new JLabel(bundle.getString("app.version")));
+        panel.add(new JLabel(bundle.getString("app.date")));
+        panel.add(new JLabel(bundle.getString("app.description")));
+        panel.add(new JLabel(bundle.getString("contact.name")));
+        panel.add(createLinkLabel(bundle.getString("contact.github"), bundle.getString("contact.github")));
+
+        JPanel contentPanel = new JPanel(new BorderLayout());
+        JLabel logoLabel = new JLabel(logoIcon);
+        contentPanel.add(logoLabel, BorderLayout.WEST);
+        contentPanel.add(panel, BorderLayout.CENTER);
+
+        JPanel wrapperPanel = new JPanel(new GridBagLayout());
+
+
+        wrapperPanel.add(contentPanel);
+
+
+
+        this.add(wrapperPanel);
     }
 
     private JLabel createLinkLabel(String text, String url) {
